e6804bcfb9de3a900133b2680b9329205e959b04
akhil-bhaskaran <akhilbhaskaran2004@gmail.com>
2024-04-28 16:58:46 +0530

    3nd

--- lib/models/todo_dataabase.dart
+++ lib/models/todo_dataabase.dart
@@ -1,22 +1,49 @@
-// import 'package:isar/isar.dart';
-// import 'package:path_provider/path_provider.dart';
-// class TodoDatabase{
-// //static late Isar _isar ;
-// static Future<void> initialise()async{
-//   final dir= await getApplicationDocumentsDirectory();
-//   dri.
+import 'package:isar/isar.dart';
+import 'package:my_todo_app/models/todo.dart';
+import 'package:path_provider/path_provider.dart';
+class TodoDatabase{
+static late Isar _isar ;
+static final List<Todo> currentTodos=[];
+//INTIALISE
+static Future<void> initialise()async{
+  final dir= await getApplicationDocumentsDirectory();
+  _isar =await Isar.open([TodoSchema], directory: dir.path);
+}
+// CREATE A TODO
+Future<void> insertTodo(String usertitle,String userdescription)async{
+
+  final NewTodo = Todo(title:usertitle , description:userdescription );
+    // save to db
+    await _isar.writeTxn(() => _isar.todos.put(NewTodo));
+    //re-read from db
+    fetchTodo();
+}
+
+// EDITING THE TODO
+// Future<void> editTodo(int id,String ?userNewTitle,String userNewTitle)async{
+//   final existingTodo=await _isar.todos.get(id);
+//   // ignore: prefer_conditional_assignment
+//   if(existingTodo==null){
+//     existingTodo=
+//   }
+
+
 // }
-//CREATE A TODO
-//void insertTodo(int id,String title,String description){
+
+// DELETING THE TODO
+Future<void> deleteTodo(int id)async{
+ await _isar.writeTxn(() => _isar.todos.delete(id));
+ await fetchTodo();
+}
+
+// DISPLAYING ALL THE TODO 
+Future<void> fetchTodo()async{
+ List<Todo> fetchedNotes = await _isar.todos.where().findAll();
+    currentTodos.clear();
+    currentTodos.addAll(fetchedNotes);
+    // notifyListeners();
+
+}
 
 
-//}
-
-//EDITING THE TODO
-
-//DELETING THE TODO
-
-//DISPLAYING ALL THE TODO 
-
-
-//}
+}
